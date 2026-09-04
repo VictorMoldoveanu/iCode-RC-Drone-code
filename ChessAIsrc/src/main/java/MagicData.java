9823b6eb25442c0c9685c2db69559c0cceb8ec24
Victor Moldoveanu <victormoldoveanu07@Gmail.com>
2026-09-04 01:36:44 -0400

    Delete ChessAIsrc directory
    Accidentally pasted the wrong thing

--- ChessAIsrc/src/main/java/MagicData.java
+++
@@ -1,9 +0,0 @@
-public class MagicData {
-    public long[] magicNumbers;
-    public long[][] attackTable;
-
-    public MagicData(long[] m, long[][] a) {
-        this.magicNumbers = m;
-        this.attackTable = a;
-    }
-}
--- ChessAIsrc/src/main/java/Pair.java
+++
@@ -1,9 +0,0 @@
-public class Pair {
-    public long blocker;
-    public long attack;
-    
-    public Pair(long b, long a){
-        blocker = b;
-        attack = a;
-    }
-}
